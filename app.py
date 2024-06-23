ff8b974ae48cda9b7718fa4568ca1bf0fc963d75
Kiuliumov <miti.bania@gmail.com>
2024-06-23 22:04:08 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import discord
+from client import client
+
+@client.event
+async def on_ready():
+    print('Logged in')
+    await client.tree.sync()
+
+@client.command(name='ping', descrpiton='This is a ping command!')
+async def ping(interaction: discord.Interaction):
+    interaction.response.send_message('Pong')
+
+
+
+
+if __name__ == '__main__':
+    client.run()
